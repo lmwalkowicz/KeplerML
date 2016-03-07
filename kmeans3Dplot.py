864f281b42f9eec507ccfb3a6d82821ba8623846
Daniel Giles <dgiles1@hawk.iit.edu>
2016-03-07 16:46:29 -0600

    bug fixes

--- kmeans3Dplot.py
+++ kmeans3Dplot.py
@@ -6,8 +6,8 @@ import scipy as sp
 from scipy import stats
 import pyfits
 import math
-import pylab as plt
-import matplotlib.pyplot as pl
+import pylab as pl
+import matplotlib.pyplot as plt
 import heapq
 from operator import xor
 import scipy.signal
@@ -31,44 +31,10 @@ from matplotlib.widgets import RadioButtons
 
 
 identifier = raw_input('Enter the identifier of the data: ')
+
+# nclusters could be obtained through the optimalK.py script
 nclusters = int(raw_input('Enter the number of clusters expected: '))
 
-def cluster_points(X, mu):
-    clusters  = {}
-    for x in X:
-        bestmukey = min([(i[0], np.linalg.norm(x-mu[i[0]])) for i in enumerate(mu)], key=lambda t:t[1])[0]
-        try:
-            clusters[bestmukey].append(x)
-        except KeyError:
-            clusters[bestmukey] = [x]
-    return clusters
- 
-def reevaluate_centers(mu, clusters):
-    newmu = []
-    keys = sorted(clusters.keys())
-    for k in keys:
-        newmu.append(np.mean(clusters[k], axis = 0))
-    return newmu
-
-def has_converged(mu, oldmu):
-    return (set([tuple(a) for a in mu]) == set([tuple(a) for a in oldmu]))
-
-def find_centers(X, K):
-    # Initialize to K random centers
-    oldmu = random.sample(X, K)
-    mu = random.sample(X, K)
-    while not has_converged(mu, oldmu):
-        oldmu = mu
-        # Assign all points in X to clusters
-        clusters = cluster_points(X, mu)
-        # Reevaluate centers
-        mu = reevaluate_centers(oldmu, clusters)
-    return(mu, clusters)
-
-def Wk(mu, clusters):
-    K = len(mu)
-    return sum([np.linalg.norm(mu[i]-c)**2/(2*len(c)) \
-               for i in range(K) for c in clusters[i]])
 
 def bounding_box(X):
     # X is the data that comes in, it's organized by lightcurve[[all features for lc 1],[features for lc2],...]
@@ -85,54 +51,10 @@ def bounding_box(X):
         
     return (xmin,xmax)
 
-def Xbb(X):
-    (xmin,xmax) = bounding_box(X)
-    Xb = [random.uniform(xmin[j],xmax[j]) for j in range(60)]
-    return Xb
-
-def gap_statistic(X):
-    (xmin,xmax) = bounding_box(X)
-    # Dispersion for real distribution
-    ### Adjust range of clusters tried here:
-    ks = range(1,10)
-    Wks = np.zeros(len(ks))
-    Wkbs = np.zeros(len(ks))
-    sk = np.zeros(len(ks))
-    for indk, k in enumerate(ks):
-        mu, clusters = find_centers(X,k)
-        Wks[indk] = np.log(Wk(mu, clusters))
-        # Create B reference datasets
-        B = 10
-        BWkbs = np.zeros(B)
-        for i in range(B):
-            xfeedformulti=[X for i in range(len(X))]
-            if __name__ == '__main__':    
-                p = Pool(7)
-                Xb = p.map(Xbb,xfeedformulti)
-                p.close()
-                p.terminate()
-                p.join()
-            #Xb = [[random.uniform(xmin[j],xmax[j]) for j in range(60)] for n in range(len(X))]
-            #for n in range(len(X)):
-            #    Xb.append([random.uniform(xmin[j],xmax[j]) for j in range(60)])
-            Xb = np.array(Xb)
-            mu, clusters = find_centers(Xb,k)
-            BWkbs[i] = np.log(Wk(mu, clusters))
-        Wkbs[indk] = sum(BWkbs)/B
-        sk[indk] = np.sqrt(sum((BWkbs-Wkbs[indk])**2)/B)
-    sk = sk*np.sqrt(1+1/B)
-    return(ks, Wks, Wkbs, sk)
-
-def optimalK(data):
-    ks, logWks, logWkbs, sk = gap_statistic(data)
-    gs = logWkbs - logWks
-    return min([k for k in range(1,len(ks)-1) if gs[k]-(gs[k+1]-sk[k+1]) >= 0])
-
 def KMeans_clusters(data,nclusters):
     # Run KMeans, get clusters
     npdata = np.array(data)
     est = KMeans(n_clusters=nclusters)
-    #est = KMeans(n_clusters=optimalK(npdata))
     est.fit(npdata)
     clusters = est.labels_
     centers = est.cluster_centers_
@@ -333,7 +255,7 @@ def plot_fit(ffeatures,clusters):
 
     plt.show()
 
-    ffeaturesID = str(identifier)+'dataByFeature.npy'
+ffeaturesID = str(identifier)+'dataByFeature.npy'
 ffeatures = np.load(ffeaturesID)
 dataID = str(identifier)+'dataByLightCurve.npy'
 data = np.load(dataID)
