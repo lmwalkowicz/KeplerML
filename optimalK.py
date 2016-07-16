1a86c30359988053fb9651d29b5b51428630cd6b
d-giles <dgiles1@hawk.iit.edu>
2016-07-16 07:48:55 -0500

    Added better documentation, working on better implementation of multiprocessing in optimalK.py

--- optimalK.py
+++ optimalK.py
@@ -89,18 +89,51 @@ def bounding_box(X):
 def gap_statistic(k):
     X = np.load('tempdata.npy')
     (xmin,xmax) = bounding_box(X)
+    """
+    # temporary npy files for multiprocessing
+    np.save('xmin',xmin)
+    np.save('xmax',xmax)
+    """
     mu, clusters = find_centers(X,k)
     Wks = np.log(Wk(mu, clusters))
     # Create B reference datasets
     B = 10
     BWkbs = np.zeros(B)
     for i in range(B):
+        # create random distribution of n points in the bounding hypercube, this is time consuming
+        # with hundreds of thousands of points. Multiprocessing solution? see comments in section, might work
         Xb = []
+        """
+        The following may be a way to reduce runtime by implementing multiprocessing to generate these reference datasets.
+        
+        def montecarlogauss(n):
+            xmin = np.load('xmin.npy')
+            xmax = np.load('xmax.npy')
+            Xbn = random.uniform(xmin[j],xmax[j])for j in range(60))
+            return Xbn
+            
+        numcpus = cpu_count()
+        if numcpus > 1:
+            # Leave 1 cpu open for system tasks.
+            usecpus = numcpus-1
+        else:
+            usecpus = 1
+            
+        p = Pool(usecpus)
+        Xb = p.map(monteCarloGauss,range(len(X)))
+        p.close()
+        p.terminate()
+        p.join()
+        """
         for n in range(len(X)):
             Xb.append([random.uniform(xmin[j],xmax[j]) for j in range(60)])
         Xb = np.array(Xb)
         mu, clusters = find_centers(Xb,k)
         BWkbs[i] = np.log(Wk(mu, clusters))
+    """
+    os.remove('xmin.npy')
+    os.remove('xmax.npy')
+    """
     Wkbs = sum(BWkbs)/B
     sk = np.sqrt(sum((BWkbs-Wkbs)**2)/B)*np.sqrt(1+1/B)
     gs = Wkbs - Wks
@@ -120,6 +153,7 @@ def optimalK(X):
     gs = np.zeros(len(ks))
     
     if __name__ == '__main__':    
+        # probably not the best way to make this run faster... but it makes it 10x faster for now
         p = Pool(10)
         gapstat = p.map(gap_statistic,ks)
         for indk,k in enumerate(ks):
