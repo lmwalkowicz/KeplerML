8430b5610fae170da2c036abca527f624b54cb48
Daniel Giles <dgiles1@hawk.iit.edu>
2016-09-21 11:56:30 -0500

    Pickles data as it's processed, removes completed file from filelist. ~3s per file.

--- keplerml.py
+++ keplerml.py
@@ -32,6 +32,7 @@ from multiprocessing import Pool,cpu_count
 #from mpl_toolkits.mplot3d import Axes3D
 #from matplotlib.widgets import RadioButtons
 import sys
+import cPickle as pickle
 if sys.version_info[0] < 3:
     from Tkinter import Tk
 else:
@@ -80,49 +81,59 @@ def save_output(identifier,fitsDir,filelist):
     Janky sorting to arrange all the data and save as a sorted numpy array (hopefully doable, if not, this can be modified pretty easily). 
     """
     files=[fitsDir+'/'+line.strip() for line in open(filelist)]
-    print("Sorting...")
+    print("Sorting output...")
     # output file was created/added to in the main part of the code.
-    outputfile = identifier+'_output'
-    outputfilesorted = identifier+'_output_sorted'
-    outputdata = [line.strip() for line in open(outputfile)]
-    outputdata.sort()
+    # outputfile = identifier+'_output'
+    outputfile = open(identifier+'_output','r+')
+    outputdata = []
+    while True:
+        try:
+            o = pickle.load(outputfile)
+        except EOFError:
+            break
+        else:
+            outputdata.append(o)
+    outputfile.close()
 
+    # As unpickled data this will only sort by the first entry, experienced issue of sorting the arrays inside the array if converting
+    # to an array or to a numpy array before sorting.
+    outputdata.sort()
+    
+    # Separate the name of the file from the data
+    files_array = [outputdata[i][0] for i in range(len(outputdata))]
+    data_array = [outputdata[i][1:] for i in range(len(outputdata))]
+    
+    # Save the output data as a numpy array
+    print("Saving as numpy array...")              
+    npdata = np.array(data_array)
+    np.save(identifier+'dataByLightCurve',npdata)
+
+    # This will save the calculated features as numpy arrays in a .npy file, which can be imported via np.load(file)
+    
     # Create a filelist of successfully processed files for filekeeping's sake
-    # Output data of processed files to a file to be converted into a numpy array
-    f = open(outputfilesorted,'w')
-    skip=len(files[0])+2
     filelist_completed=identifier+'_filelist_completed'
-    fl = open(filelist_completed,'w')
-    for line in outputdata:
-        # Write the data without the filepath
-        f.write(line[skip:]+'\n')
-        # Write the completed filelist w/o full filepath, just fits file id
-        fl.write(line[len(fitsDir):skip-2]+'\n')
-    f.close()
+    fl=open(filelist_completed,'w')    
+    for fitsfile in files_array:fl.write(fitsfile+'\n')
+    
     fl.close()
     
     ### Primarily for a failed run ###
     # Remove successfully processed files from filelist so they don't need to be reprocessed.
-    for line in outputdata:
-        if line in files:files.remove(line)
-    fl_update=open(filelist,'w')
-    for line in files:
-        fl_update.write(line+'\n')
-    fl_update.close()
-
-
-    # Save the output data as a numpy array
-    print("Saving as numpy array...")              
-    npdata = np.loadtxt(outputfilesorted,delimiter=',')
-    np.save(identifier+'dataByLightCurve',npdata)
-
-    # This will save the calculated features as numpy arrays in a .npy file, which can be imported via np.load(file)
-    #np.save(identifier+'dataByLightCurve',data)
-
+    for line in files_array:
+        if fitsDir+'/'+line in files:files.remove(fitsDir+'/'+line)
+    if files!=[]:
+        fl_update=open(filelist,'w')
+        for line in files:
+            fl_update.write(line.replace(fitsDir+'/',"")+'\n')
+        fl_update.close()
+    else:
+        os.system('rm %s'%filelist)
+    
     kml_log = identifier+'_kml_log'
 
     totalTime = datetime.now()-startTime
     os.system('echo %s>> %s'%(totalTime,kml_log))
+
     return
 
 def calc_outliers_pts(t, nf):
@@ -425,11 +436,13 @@ def fcalc(nfile):
 
     This takes about 3-4 times longer, but the data output is more controlled and if it fails in the middle there's still data that can be retrieved (read: doesn't need to be reprocessed and should be removed from the filelist).
     """
-    
-    ndatastr ='%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s'%(longtermtrend, meanmedrat, skews, varss, coeffvar, stds, numoutliers, numnegoutliers, numposoutliers, numout1s, kurt, mad, maxslope, minslope, meanpslope, meannslope, g_asymm, rough_g_asymm, diff_asymm, skewslope, varabsslope, varslope, meanabsslope, absmeansecder, num_pspikes, num_nspikes, num_psdspikes, num_nsdspikes,stdratio, pstrend, num_zcross, num_pm, len_nmax, len_nmin, mautocorrcoef, ptpslopes, periodicity, periodicityr, naiveperiod, maxvars, maxvarsr, oeratio, amp, normamp,mbp, mid20, mid35, mid50, mid65, mid80, percentamp, magratio, sautocorrcoef, autocorrcoef, flatmean, tflatmean, roundmean, troundmean, roundrat, flatrat)
-
-    nfile=nfile.replace(fitsDir,"")
-    os.system('echo %s, %s>>%s'%(nfile,ndatastr,identifier+'_output'))
+    nfile=nfile.replace(fitsDir+'/',"")
+    ndata = [nfile,longtermtrend, meanmedrat, skews, varss, coeffvar, stds, numoutliers, numnegoutliers, numposoutliers, numout1s, kurt, mad, maxslope, minslope, meanpslope, meannslope, g_asymm, rough_g_asymm, diff_asymm, skewslope, varabsslope, varslope, meanabsslope, absmeansecder, num_pspikes, num_nspikes, num_psdspikes, num_nsdspikes,stdratio, pstrend, num_zcross, num_pm, len_nmax, len_nmin, mautocorrcoef, ptpslopes, periodicity, periodicityr, naiveperiod, maxvars, maxvarsr, oeratio, amp, normamp,mbp, mid20, mid35, mid50, mid65, mid80, percentamp, magratio, sautocorrcoef, autocorrcoef, flatmean, tflatmean, roundmean, troundmean, roundrat, flatrat]
+    if not os.path.isfile(identifier+'_output'):
+        open(identifier+'_output','a').close()
+    id_output=open(identifier+'_output','a')
+    pickle.dump(ndata,id_output)
+    id_output.close()
     
     #return longtermtrend, meanmedrat, skews, varss, coeffvar, stds, numoutliers, numnegoutliers, numposoutliers, numout1s, kurt, mad, maxslope, minslope, meanpslope, meannslope, g_asymm, rough_g_asymm, diff_asymm, skewslope, varabsslope, varslope, meanabsslope, absmeansecder, num_pspikes, num_nspikes, num_psdspikes, num_nsdspikes,stdratio, pstrend, num_zcross, num_pm, len_nmax, len_nmin, mautocorrcoef, ptpslopes, periodicity, periodicityr, naiveperiod, maxvars, maxvarsr, oeratio, amp, normamp,mbp, mid20, mid35, mid50, mid65, mid80, percentamp, magratio, sautocorrcoef, autocorrcoef, flatmean, tflatmean, roundmean, troundmean, roundrat, flatrat
 
@@ -473,11 +486,20 @@ if filelist:
     print('Select the fits files location (must all be in one directory)')
     Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
     fitsDir = askdirectory() # show an "Open" dialog box and return the path to the selected file
-
-identifier=str(raw_input('Choose a unique identifier: '))
-
-feature_calc(filelist)
-
-save_output(identifier,fitsDir,filelist)
-
-print("Done")
+    if fitsDir:
+        identifier=str(raw_input('Choose a unique identifier: '))
+        outputfile = identifier+'_output'
+        while os.path.isfile(outputfile):
+            replace = raw_input('Output exists for identifier, replace? (y/n) ')
+            if replace == 'y':
+                os.system('rm %s'%outputfile)
+            else:
+                identifier=str(raw_input('Choose a unique identifier: '))
+                outputfile = identifier+'_output'
+        
+        feature_calc(filelist)
+        kml_log = identifier+'_kml_log'
+        totalTime = datetime.now()-startTime
+        os.system('echo %s>> %s'%(totalTime,kml_log))
+        save_output(identifier,fitsDir,filelist)
+        print("Done")
